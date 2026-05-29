9e8ee5d8f37a68a67f000095392b89087c929128
Sabson <saba.chkadua1@gmail.com>
2026-05-29 22:00:54 +0000

    Always recreate .env on startup

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -1,20 +1,12 @@
 #!/bin/sh
 set -e
+
 echo "DATABASE_URL value: $DATABASE_URL"
-
 echo "🔧 Setting up environment variables..."
-if [ ! -f /app/env/.env ]; then
-  echo ".env file not found, creating a new one."
-  cp /app/env/.env.default /app/env/.env
-fi
-
-if [ ! -L /app/.env ]; then
-  echo "Creating symlink to .env file..."
-  ln -s /app/env/.env /app/.env
-fi
 
 rm -f /app/.env /app/env/.env
-echo "DATABASE_URL=\"$DATABASE_URL\"" > /app/env/.env
+cp /app/env/.env.default /app/env/.env
+echo "DATABASE_URL=\"$DATABASE_URL\"" >> /app/env/.env
 ln -sf /app/env/.env /app/.env
 
 echo "✅ Applying 'prisma db push'..."
