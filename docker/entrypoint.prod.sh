bb14d468796789ae7d24dae8dc1776f05fbd90d1
Sabson <saba.chkadua1@gmail.com>
2026-05-29 21:19:23 +0000

    Always inject DATABASE_URL

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -8,16 +8,18 @@ else
   echo ".env file not found, creating a new one."
   cp /app/env/.env.default /app/env/.env
 fi
+
 if [ ! -L /app/.env ]; then
   echo "Creating symlink to .env file..."
   ln -s /app/env/.env /app/.env
-  if [ ! -z "$DATABASE_URL" ]; then
-    sed -i "s|DATABASE_URL=.*|DATABASE_URL=$DATABASE_URL|" /app/env/.env
-  fi
 else
   echo "Symlink to .env file already exists."
 fi
 
+if [ ! -z "$DATABASE_URL" ]; then
+  echo "Injecting DATABASE_URL..."
+  sed -i "s|DATABASE_URL=.*|DATABASE_URL=$DATABASE_URL|" /app/env/.env
+fi
 
 echo "✅ Database available. Applying 'prisma db push'..."
 npx dotenv -e /app/.env -- npx prisma db push
