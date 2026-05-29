cb0e744b185bade68c2efa3dad6cf18ffdb87f33
Sabson <saba.chkadua1@gmail.com>
2026-05-29 21:51:37 +0000

    Rewrite .env with correct DATABASE_URL

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -2,9 +2,7 @@
 set -e
 
 echo "🔧 Setting up environment variables..."
-if [ -f /app/env/.env ]; then
-  echo ".env file found."
-else
+if [ ! -f /app/env/.env ]; then
   echo ".env file not found, creating a new one."
   cp /app/env/.env.default /app/env/.env
 fi
@@ -12,15 +10,19 @@ fi
 if [ ! -L /app/.env ]; then
   echo "Creating symlink to .env file..."
   ln -s /app/env/.env /app/.env
-else
-  echo "Symlink to .env file already exists."
 fi
 
 echo "✅ Applying 'prisma db push'..."
-DATABASE_URL="$DATABASE_URL" npx prisma db push
+unset DATABASE_URL
+export DATABASE_URL="$DATABASE_URL"
+rm -f /app/.env /app/env/.env
+echo "DATABASE_URL=\"$DATABASE_URL\"" > /app/env/.env
+ln -sf /app/env/.env /app/.env
+
+npx prisma db push
 
 echo "📦 Running data and file migrations..."
-DATABASE_URL="$DATABASE_URL" node /app/migration-scripts/migrate.js
+node /app/migration-scripts/migrate.js
 
 echo "🚀 Starting application..."
 exec npm run serve
