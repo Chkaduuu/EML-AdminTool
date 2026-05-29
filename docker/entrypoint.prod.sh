4c0dadb878e395ee23577478854da3be77217538
Sabson <saba.chkadua1@gmail.com>
2026-05-29 21:57:05 +0000

    Debug DATABASE_URL

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+echo "DATABASE_URL value: $DATABASE_URL"
 
 echo "🔧 Setting up environment variables..."
 if [ ! -f /app/env/.env ]; then
@@ -12,13 +13,11 @@ if [ ! -L /app/.env ]; then
   ln -s /app/env/.env /app/.env
 fi
 
-echo "✅ Applying 'prisma db push'..."
-unset DATABASE_URL
-export DATABASE_URL="$DATABASE_URL"
 rm -f /app/.env /app/env/.env
 echo "DATABASE_URL=\"$DATABASE_URL\"" > /app/env/.env
 ln -sf /app/env/.env /app/.env
 
+echo "✅ Applying 'prisma db push'..."
 npx prisma db push
 
 echo "📦 Running data and file migrations..."
